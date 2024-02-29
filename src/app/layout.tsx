fb8a306562ecee1aff3e6f27bf9b585d76455924
premvarma2002 <vakilverma1982@gmail.com>
2024-02-29 11:49:53 +0530

    Add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./provider";
+import PageLayout from "./page-layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-        {children}
+          <PageLayout>{children}</PageLayout>
         </Providers>
       </body>
     </html>
---
+++ src/app/page-layout.tsx
@@ -0,0 +1,17 @@
+import { Footer } from '@/components/client/footer';
+import { Navbar } from '@/components/client/navbar';
+import React from 'react'
+
+const PageLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div className='relative flex flex-col text-black bg-white' id='app-container'>
+       <main className='flex flex-col relative'>
+        <Navbar/>
+       <section className='h-full flex-1'>{children}</section> 
+       <Footer/>
+       </main>    
+    </div>
+  )
+}
+
+export default PageLayout;
